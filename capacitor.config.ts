efcec7c00b701b9937269baa8e4dc6602c5a2617
Gabriel Escobar <gabrielescobar283@gmail.com>
2026-05-22 00:42:44 -0500

    Agregando Cambios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'poemas-crud',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      androidSplashResourceName: "splash", // Obliga a buscar el archivo splash.png
+      backgroundColor: "#FFFFFF",         // Cambia este valor por el color de tu fondo (ej. #FFFFFF para blanco)
+      androidScaleType: "CENTER_CROP"     // Estira la imagen para cubrir la pantalla
+    },
+  },
 };
 
 export default config;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl:"https://emloexxyutsudfkwyumv.supabase.co",
+  supabaseKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtbG9leHh5dXRzdWRma3d5dW12Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTMyNTQ5NSwiZXhwIjoyMDk0OTAxNDk1fQ.qjYkmpkJtm1-QM8_C-Id_wvyWLmFvoQds5w18suoYEQ"
 };
